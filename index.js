c7f3bd929d7cabf7bae30286f403019f55696dd5
SURYA-200346 <surya.jsdev@gmail.com>
2025-11-06 01:43:06 +0530

    new files added

---
+++ index.js
@@ -0,0 +1,32 @@
+const skillSection = document.querySelector('.skill-section');
+const progressBars = document.querySelectorAll('.progressbar');
+let animated = false;
+
+window.addEventListener('scroll', () => {
+    const sectionPosition = skillSection.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+    if (sectionPosition < screenHeight - 100 && !animated) {
+        animated = true;
+        progressBars.forEach(bar => {
+            const value = bar.getAttribute('data-progress');
+            bar.style.width = value;
+        });
+    }
+});
+
+// menu Tab
+const menu = document.getElementById("menutab");
+const navlink = document.getElementById("navbar")
+menu.addEventListener("click", () => {
+    navlink.classList.toggle("active");
+});
+// auto close tab
+const links = document.querySelectorAll("#navbar a");
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navlink.classList.remove("active");
+    });
+});
+
+
+
